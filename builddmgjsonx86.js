2e21eeedd5aa3ca06d84aef2b82a7331353830da
xianfei <xianfei@bupt.cn>
2024-07-04 00:57:16 +0800

    fixed github actions

---
+++ builddmgjsonx86.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const path = require('path');
+
+const path2 = './OutApp/packages';
+
+// 检查文件夹是否存在
+if (!fs.existsSync(path2)) {
+    // 创建文件夹
+    fs.mkdirSync(path2, { recursive: true });
+    console.log(`目录 ${path2} 已创建`);
+} else {
+    console.log(`目录 ${path2} 已存在`);
+}
+
+// 配置对象
+let config = {
+  title: 'SysMocap-macOS-x64',
+  format: 'UDZO',
+  contents: [
+    { x: 448, y: 344, type: 'link', path: '/Applications' },
+    {
+      x: 192,
+      y: 344,
+      type: 'file',
+      path: '/Users/xianfei/Documents/GitHub/SysMocap/OutApp/SysMocap-darwin-x64/SysMocap.app'
+    }
+  ],
+  background: '/Users/xianfei/Documents/GitHub/SysMocap/node_modules/electron-installer-dmg/resources/mac/background.png',
+  icon: '/Users/xianfei/Documents/GitHub/SysMocap/icons/sysmocap.icns',
+  'icon-size': 80
+};
+
+// 获取当前目录
+const currentDir = process.cwd();
+
+// 替换路径函数
+function replacePath(obj, oldPath, newPath) {
+  for (let key in obj) {
+    if (typeof obj[key] === 'string' && obj[key].includes(oldPath)) {
+      obj[key] = obj[key].replace(oldPath, newPath);
+    } else if (typeof obj[key] === 'object') {
+      replacePath(obj[key], oldPath, newPath);
+    }
+  }
+}
+
+// 替换路径
+replacePath(config, '/Users/xianfei/Documents/GitHub/SysMocap', currentDir);
+
+// 输出新的配置对象
+const outputPath = path.join(currentDir, 'dmgcfg.json');
+fs.writeFileSync(outputPath, JSON.stringify(config, null, 2), 'utf-8');
